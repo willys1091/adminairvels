dbd2e368cb9b82076cf7e6320d8af364ec946757
willys <w1llys_910710@yahoo.co.id>
2021-01-01 23:33:25 +0700

    attribute controller category, country, destination added

---
+++ app/Http/Controllers/DestinationController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Session;
+use App\Models\destination;
+
+class DestinationController extends Controller{
+    public function index(){
+        //
+    }
+
+    public function create(){
+        //
+    }
+
+    public function store(Request $request){
+        //
+    }
+
+    public function show($id){
+        //
+    }
+
+    public function edit($id){
+        //
+    }
+
+    public function update(Request $request, $id){
+        //
+    }
+
+    public function destroy($id){
+        //
+    }
+}
--- app/Http/Controllers/RoleController.php
+++ app/Http/Controllers/RoleController.php
@@ -3,8 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\role;
 use Session;
+use App\Models\role;
 
 class RoleController extends Controller{
     public function index(){
